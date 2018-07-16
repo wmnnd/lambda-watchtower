e29850f5f0c9b46e519722ea280964e28ddb6bf1
Philipp <git@pentacent.com>
2018-07-16 20:19:14 +0200

    Add and apply ESLint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const https = require("https")
 const http = require("http")
-const net = require('net')
+const net = require("net")
 const {hrtime} = process
 const AWS = require("aws-sdk")
 const cloudwatch = new AWS.CloudWatch()
@@ -10,14 +10,14 @@ const hrDiff = (start, end) => hrToMs(end) - hrToMs(start)
 const timingsDiff = (timings, key1, key2) => timings[key1] && timings[key2] && hrDiff(timings[key1], timings[key2])
 
 const processTimings = function(timings) {
-	return {
-		lookup: timingsDiff(timings, "start", "lookup"),
-		connect: timingsDiff(timings, "lookup", "connect"), 
-		secureConnect: timingsDiff(timings, "connect", "secureConnect"),
-		readable: timingsDiff(timings, "secureConnect", "readable") || timingsDiff(timings, "connect", "readable"),
+    return {
+        lookup: timingsDiff(timings, "start", "lookup"),
+        connect: timingsDiff(timings, "lookup", "connect"), 
+        secureConnect: timingsDiff(timings, "connect", "secureConnect"),
+        readable: timingsDiff(timings, "secureConnect", "readable") || timingsDiff(timings, "connect", "readable"),
         close: timingsDiff(timings, "readable", "close"),
         total: timingsDiff(timings, "start", "close")
-	}
+    }
 }
 
 const createRequest = function(url, callback) {
@@ -44,92 +44,89 @@ exports.handler = function(event, context, callback) {
     const targets = event.targets
     if (!targets) callback("No targets given")
 
-	const requests = targets.map(target => new Promise((resolve, reject) => {
-		const data = {
+    const requests = targets.map(target => new Promise((resolve, reject) => {
+        const data = {
             name: target.name || target.url,
             timings: {
                 start: hrtime()
             }
         }
-		if (target.type === undefined || target.type === 'http/s') {
-			const request = createRequest(target.url, response => {
-				data.statusCode = response.statusCode
-				response.once("readable", () => data.timings.readable = hrtime())
-				response.once("end", () => data.timings.end = hrtime())
-			})
-			request.setTimeout(1)
-			const timeout = setTimeout(() => request.abort(), event.timeout || 2000)
-			request.on("socket", socket => {
-				socket.on("lookup", () => data.timings.lookup = hrtime())
-				socket.on("connect", () => data.timings.connect = hrtime())
-				socket.on("secureConnect", () => data.timings.secureConnect = hrtime())
-			})
-			request.on("close", () => {
-				data.timings.close = hrtime()
-				data.durations = processTimings(data.timings)
-				clearTimeout(timeout)
-				resolve(data)
-			})
-			request.on("error", () => {
-				data.timings.close = hrtime()
-				data.durations = processTimings(data.timings)
-				data.statusCode = typeof data.statusCode !== "undefined" ? data.statusCode : 0
-				clearTimeout(timeout)
-				resolve(data)
-			})
-		} else if (target.type === 'port') {
+        if (target.type === undefined || target.type === "http/s") {
+            const request = createRequest(target.url, response => {
+                data.statusCode = response.statusCode
+                response.once("readable", () => data.timings.readable = hrtime())
+                response.once("end", () => data.timings.end = hrtime())
+            })
+            request.setTimeout(1)
+            const timeout = setTimeout(() => request.abort(), event.timeout || 2000)
+            request.on("socket", socket => {
+                socket.on("lookup", () => data.timings.lookup = hrtime())
+                socket.on("connect", () => data.timings.connect = hrtime())
+                socket.on("secureConnect", () => data.timings.secureConnect = hrtime())
+            })
+            request.on("close", () => {
+                data.timings.close = hrtime()
+                data.durations = processTimings(data.timings)
+                clearTimeout(timeout)
+                resolve(data)
+            })
+            request.on("error", () => {
+                data.timings.close = hrtime()
+                data.durations = processTimings(data.timings)
+                data.statusCode = typeof data.statusCode !== "undefined" ? data.statusCode : 0
+                clearTimeout(timeout)
+                resolve(data)
+            })
+        } else if (target.type === "port") {
+            if(target.url.startsWith("http://") || target.url.startsWith("https://")){
+                reject("http url for non http check")
+            }
 			
-			if(target.url.startsWith('http://') || target.url.startsWith('https://')){
-				reject("http url for non http check")
-			}
-			
-			const socket = new net.Socket()
+            const socket = new net.Socket()
 
-			socket.setTimeout(event.timeout || 2000);
+            socket.setTimeout(event.timeout || 2000)
 
-			socket.on('connect',() => {
-				data.timings.connect = hrtime()
-			})
-			socket.on('lookup',() => {
-				data.timings.lookup = hrtime()
-			})
-			socket.on('data',() => {
-				data.timings.readable = hrtime()
-				socket.end();
-			})
-			socket.on('end',() => {
-				data.timings.end = hrtime()
-			})
-			socket.on('error',() => {
-				data.timings.close = hrtime()
-				data.durations = processTimings(data.timings)
-				data.statusCode = -1
-				socket.destroy()
-				resolve(data)
-			});
-			socket.on('timeout', () => {
-				data.timings.close = hrtime()
-				data.durations = processTimings(data.timings)
-				data.statusCode = -1
-				socket.destroy()
-				resolve(data)
-			});
-			socket.on('close', () => {
-				data.timings.close = hrtime()
-				data.durations = processTimings(data.timings)
-				data.statusCode = 0
-				socket.destroy()
-				resolve(data)
-			})
+            socket.on("connect",() => {
+                data.timings.connect = hrtime()
+            })
+            socket.on("lookup",() => {
+                data.timings.lookup = hrtime()
+            })
+            socket.on("data",() => {
+                data.timings.readable = hrtime()
+                socket.end()
+            })
+            socket.on("end",() => {
+                data.timings.end = hrtime()
+            })
+            socket.on("error",() => {
+                data.timings.close = hrtime()
+                data.durations = processTimings(data.timings)
+                data.statusCode = -1
+                socket.destroy()
+                resolve(data)
+            })
+            socket.on("timeout", () => {
+                data.timings.close = hrtime()
+                data.durations = processTimings(data.timings)
+                data.statusCode = -1
+                socket.destroy()
+                resolve(data)
+            })
+            socket.on("close", () => {
+                data.timings.close = hrtime()
+                data.durations = processTimings(data.timings)
+                data.statusCode = 0
+                socket.destroy()
+                resolve(data)
+            })
 
-			socket.connect(target.port, target.url, () => {
-			});
-			
-			
-		}
-	}))
+            socket.connect(target.port, target.url, () => {
+            })
+        }
+    }))
     
-	return Promise.all(requests).then(results => {
+    return Promise.all(requests).then(results => {
         const timestamp = new Date()
         const includedTimings = event.logTimings || ["readable", "total"]
         const metricData = results.map(result => {
@@ -143,7 +140,7 @@ exports.handler = function(event, context, callback) {
                 }
             })
             return [{
-                MetricName: `status`,
+                MetricName: "status",
                 Dimensions: [{Name: result.name, Value: "HTTP Status"}],
                 Value: result.statusCode,
                 Timestamp: timestamp
@@ -151,19 +148,18 @@ exports.handler = function(event, context, callback) {
         }).reduce((acc, val) => [...acc, ...val], [])
 
         const params = {
-			Namespace: event.namespace || "Watchtower",
-			MetricData: metricData,
+            Namespace: event.namespace || "Watchtower",
+            MetricData: metricData,
         }
         return cloudwatch.putMetricData(params).promise()
-		.then(data => {
-			callback(null,data);
-		})
-		.catch(error => {
-			callback(error,null);
-		});
+            .then(data => {
+                callback(null,data)
+            })
+            .catch(error => {
+                callback(error,null)
+            })
 
-	})
-	.catch(error => {
-		callback(error)
-	})
+    }).catch(error => {
+        callback(error)
+    })
 }
